7da21e1a58956f2f7a6e99f4a84aeb7ff0f7fee3
nic <nwanavit@hatari.cc>
2020-01-24 21:19:18 +0700

    change copy to creating symlink

--- startSite.sh
+++ startSite.sh
@@ -1,4 +1,4 @@
-cp ../template.yml ./ && \
+ln -s ../template.yml ./ && \
 bash ./checkTemplate.sh && \
 aws cloudformation create-stack --stack-name exampleSiteUdacity --template-body file://template.yml && \
 bash ./describeStack.sh
